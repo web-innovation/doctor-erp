171917edeed369a1a8027ec21d1092379cb47e1d
gagangarg <er.gagangarg18@gmail.com>
2026-02-24 02:37:11 +0530

    changes

---
+++ server/prisma-postgres/migrations/20260223152000_add_bill_module_defaults/migration.sql
@@ -0,0 +1,7 @@
+﻿-- Add per-module GST and discount defaults to Bill (PostgreSQL)
+ALTER TABLE "Bill" ADD COLUMN IF NOT EXISTS "defaultConsultationGstPercent" DOUBLE PRECISION;
+ALTER TABLE "Bill" ADD COLUMN IF NOT EXISTS "defaultPharmacyGstPercent" DOUBLE PRECISION;
+ALTER TABLE "Bill" ADD COLUMN IF NOT EXISTS "defaultLabTestGstPercent" DOUBLE PRECISION;
+ALTER TABLE "Bill" ADD COLUMN IF NOT EXISTS "defaultConsultationDiscountPercent" DOUBLE PRECISION;
+ALTER TABLE "Bill" ADD COLUMN IF NOT EXISTS "defaultPharmacyDiscountPercent" DOUBLE PRECISION;
+ALTER TABLE "Bill" ADD COLUMN IF NOT EXISTS "defaultLabTestDiscountPercent" DOUBLE PRECISION;
